229f216b2abee535c8696464f3293c56b3b4b45a
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-07-31 20:08:12 +0500

    something in writercontroller exists

--- Controllers/WrtitersController.cs
+++ Controllers/WrtitersController.cs
@@ -1,6 +1,7 @@
 ﻿using FirstWeb.DTOS;
 using FirstWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
 
 namespace FirstWeb.Controllers;
 
@@ -11,6 +12,26 @@ public class WrtitersController : ControllerBase
 {
     private static int count = 1;
     private static List<Writer> Writers = new();
+    private const string connection = "Data source = mydata.db; Version =3";
+
+    private readonly List<string> createTableQueries = new()
+    {
+        "CREATE TABLE IF NOT EXISTS Writers (Id INTEGER PRIMARY KEY AUTOINCREMENT, FullName TEXT, BirthDate DATETIME)",
+        "CREATE TABLE IF NOT EXISTS WriterBooks (WriterId INT NOT NULL, BookId INT NOT NULL, PRIMARY KEY (WriterId, BookId), FOREIGN KEY (WriterId) REFERENCES Writers (Id), FOREIGN KEY (BookId) REFERENCES Books (Id);"
+    };
+
+    private void CreateTables()
+    {
+        using SQLiteConnection conn = new SQLiteConnection(connection);
+
+        conn.Open();
+
+        foreach (var query in createTableQueries)
+        {
+            using SQLiteCommand command = new SQLiteCommand(query, conn);
+            command.ExecuteNonQuery();
+        }
+    }
 
     /// <summary>
     /// Siz yangi yozuvchi modelini yaratishiz mumkin!
@@ -18,20 +39,20 @@ public class WrtitersController : ControllerBase
     /// <param name="writerModel"></param>
     /// <remarks>
     /// POST/TODO
-///      {
-///          "fullName": "string",
-///          "birthDate": "2023-07-27T13:25:33.673Z",
-///          "books": [
-///            {
-///              "id": 0,
-///              "name": "string",
-///              "price": 0,
-///              "authorName": "string",
-///              "writerId": 0,
-///              "genres": [0]
-///            }
-///                 ]
-///     }       
+    ///      {
+    ///          "fullName": "string",
+    ///          "birthDate": "2023-07-27T13:25:33.673Z",
+    ///          "books": [
+    ///            {
+    ///              "id": 0,
+    ///              "name": "string",
+    ///              "price": 0,
+    ///              "authorName": "string",
+    ///              "writerId": 0,
+    ///              "genres": [0]
+    ///            }
+    ///                 ]
+    ///     }       
     /// </remarks>
     /// <response code="200">Returns the newly created Writer</response>
 
@@ -39,16 +60,20 @@ public class WrtitersController : ControllerBase
     [ProducesResponseType(typeof(Writer),200)]
     public IActionResult CreateWriter(CreateWriterDTO writerModel)
     {
-        var writer = new Writer()
-        {
-            Id = count++,
-            FullName = writerModel.FullName,
-            BirthDate = writerModel.BirthDate,
-            Books = writerModel.Books
-        };
+        using SQLiteConnection conn = new SQLiteConnection(connection);
+        conn.Open();
 
-        Writers.Add(writer);
-        return Ok(writer);
+        string insertQuery = "INSERT INTO Writers (FullName, BirthDate) VALUES (@fullname, @birthdate)";
+
+        using (SQLiteCommand command = new SQLiteCommand(insertQuery, conn))
+        {
+            command.Parameters.AddWithValue("@fullname", $"{writerModel.FullName}");
+            command.Parameters.AddWithValue("@birthdate", writerModel.BirthDate);
+
+            command.ExecuteNonQuery();
+        }
+
+        return Ok();
 
     }
 
